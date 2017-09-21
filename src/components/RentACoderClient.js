5e2e8925a41366899368d975ace3f9a4c03432d5
Mauricio Coniglio <mauricio.coniglio@moove-it.com>
2017-09-21 19:21:40 -0300

    Initial commit

---
+++ src/components/RentACoderClient.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RentACoderClient = () => (
+  <div className="rentACoderClient">
+    <h1>Rent A Coder</h1>
+  </div>
+);
+
+export default RentACoderClient;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RentACoderClient from './components/RentACoderClient';
+
+ReactDOM.render(
+  <RentACoderClient />,
+  document.getElementById('root')
+);
